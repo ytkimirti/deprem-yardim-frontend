a7c872b5a16f792c39b4bda004c70156993a3550
Yusuf <yusuftaha9@gmail.com>
2023-02-08 19:03:27 +0300

    fix(googlemaps): google maps get directions url not working
    resource: https://developers.google.com/maps/documentation/urls/get-started#directions-action
    example url: https://www.google.com/maps/dir/?api=1&destination=37.6006049%2C36.9030401

--- components/UI/Drawer/Drawer.tsx
+++ components/UI/Drawer/Drawer.tsx
@@ -49,7 +49,7 @@ export const openAppleMapsUrl = (lat: number, lng: number) => {
 
 export const openGoogleMapsDirectionUrl = (lat: number, lng: number) => {
   window.open(
-    `https://www.google.com/maps?saddr=My+Location&daddr=${lat},${lng}`,
+    `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}`,
     "_blank"
   );
 };
